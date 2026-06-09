8c306a20911599fe60b944e96170f915e1616e19
UnishLaxamba <ylaksamba@gmail.com>
2026-06-10 00:15:15 +0545

    Added the data set to train the model, created the required array inside the code

--- LBPH(AI_Framework).py
+++ LBPH(AI_Framework).py
@@ -6,3 +6,15 @@ import numpy as np
 
 #Importing the OS module
 import os
+
+#Loading the Haarcascade xml file
+face_cascade = cv.CascadeClassifier(cv.data.haarcasades + 'haarcascade_frontalface_default.xml')
+
+#Loading the LBPH recognizer
+recognizer = cv.face.LBPHFaceRecognizer_create()
+
+#Initializing the data set folder path
+data_set_path = 'Tranning_Images'
+
+Faces = []
+Labels = []
